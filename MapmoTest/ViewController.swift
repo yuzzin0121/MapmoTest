faeb33a5db00c58b2dd7872f36f5b7ed1236c1fa
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-03-06 23:10:18 +0900

    Initial Commit

---
+++ MapmoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapmoTest
+//
+//  Created by 조유진 on 3/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
